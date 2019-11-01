b7d929b8cffe5aca1e8273c1b23335ffd335925d
François Mockers <mockersf@gmail.com>
2019-11-01 03:51:16 +0100

    :white_check_mark: add test on client creation

---
+++ tests/test_client.rs
@@ -0,0 +1,19 @@
+use spectral::prelude::*;
+
+#[test]
+fn can_create_with_valid_url() {
+    let mlflow = mlflow_api::MlflowClient::new(
+        std::env::var("MLFLOW_URL").unwrap_or_else(|_| "http://127.0.0.1:5000".to_string()),
+    );
+
+    assert_that!(mlflow).is_ok();
+}
+
+#[test]
+fn cant_create_with_invalid_url() {
+    let mlflow = mlflow_api::MlflowClient::new(
+        std::env::var("MLFLOW_URL").unwrap_or_else(|_| "not-a-url".to_string()),
+    );
+
+    assert_that!(mlflow).is_err();
+}
